96a5271ca57a04f87f3a00d0c154536828ec0c77
Elijah Zane Chapman <ezc9@outlook.com>
2026-04-07 18:12:15 -0500

    This adds ratelimiting and ensures that we can connect to the mongoDb database before starting our server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,20 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./utils/db.js";
 import dotenv from "dotenv";
+import { rateLimiter } from "./middleware/rateLimit.js";
 
 dotenv.config();
-
-connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(rateLimiter)
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`)
+    })
+});
